90e8d1204893a8ce491e1e0f5aef1fe8ec069d56
Frank Cox <frank.l.cox@gmail.com>
2009-10-16 19:03:16 +0000

    Import of FRANKCOX/Geo-ReadGRIB-0.98_2 from CPAN.
    gitpan-cpan-distribution: Geo-ReadGRIB
    gitpan-cpan-version:      0.98_2
    gitpan-cpan-path:         FRANKCOX/Geo-ReadGRIB-0.98_2.tar.gz
    gitpan-cpan-author:       FRANKCOX
    gitpan-cpan-maturity:     developer

--- lib/Geo/ReadGRIB.pm
+++ lib/Geo/ReadGRIB.pm
@@ -14,7 +14,7 @@ use strict;
 use IO::File;
 use Carp;
 
-our $VERSION = 0.98_10;
+our $VERSION = 0.98_20;
 use Geo::ReadGRIB::PlaceIterator;
 
 my $LIB_DIR = "./";
@@ -899,9 +899,9 @@ __END__
 
 *****************************************************
 
-THIS IS A TEST VERSION WITH NEW SUPPORT CMS GRIBS -- 
+THIS IS A TEST VERSION WITH NEW SUPPORT CMS GRIBS 
 
-FULL TESTING HAS NOT BEEN DONE --
+THERE ARE MANY CHANGES AND FULL TESTING HAS NOT BEEN DONE
 
 USE V0.98 FOR ANYTHING EXCEPT TESTING
 
